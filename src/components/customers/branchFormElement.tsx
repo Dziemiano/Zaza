c26284f59cba5f50101a16fde33158b4c13d6824
Patryk Dziemianowski <patryk.dziemianowski@gmail.com>
2024-08-01 08:25:21 +0200

    remove

--- src/components/customers/branchFormElement.tsx
+++ src/components/customers/branchFormElement.tsx
@@ -306,7 +306,6 @@ export function BranchFormElement({ name }: BranchFormElementProps) {
         >
           Dodaj filię
         </Button>
-        <code>{JSON.stringify(fields)}</code>
       </CardContent>
     </Card>
   );
